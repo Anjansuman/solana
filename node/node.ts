5685a209fc875950b00e687e3930a082bf468eb8
Anjansuman <anjansuman80@gmail.com>
2025-12-15 19:43:12 +0530

    added p2p server and updated node for communication.

--- node/node.ts
+++ node/node.ts
@@ -6,15 +6,17 @@ import type BlockType from "../types/block.type";
 import type { ProgramType } from "../types/program.type";
 import type { TransactionType } from "../types/transaction.type";
 import type Block from "../block/block";
+import type { PeerType } from "../types/peer.type";
+import rpc_client from "../rpc/rpc.client";
 
 
 export default class Node {
 
     private node_id: number;
     private total_nodes: number;
+    private peers: Map<number, PeerType>;
 
     private slot_duration: number;
-    private slot_timer: NodeJS.Timeout;
     private current_slot: number;
 
     private mem_pool: TransactionType[];
@@ -26,11 +28,20 @@ export default class Node {
     private program_registry: Map<string, ProgramType>; // address -> program
     private runtime: Runtime;
 
+    // for now assume that only the 3 initial nodes can become the leader, later we'll add dynamic leaders based of nodes
+    private validators: PeerType[] = [
+        { nodeId: 0, rpc: '', p2p: '' },
+        { nodeId: 1, rpc: '', p2p: '' },
+        { nodeId: 2, rpc: '', p2p: '' },
+    ]
+
+    private syncing: boolean = false;
 
     constructor(config: any) {
 
         this.node_id = config.node_id;
         this.total_nodes = config.total_nodes;
+        this.peers = config.peers; // start with some nodes eventually add more after starting
 
         this.slot_duration = config.slot_duration;
         this.current_slot = config.current_slot;
@@ -50,9 +61,48 @@ export default class Node {
 
     }
 
-    public start() {
+    public async start() {
+        await this.sync_ledger();
+        this.start_slot_clock();
+    }
+
+    private async sync_ledger() {
+        // make the syncing ledger true to not assign this node as a leader until syncing completes
+        this.syncing = true;
+
+        if(this.peers.size === 0) {
+            // genesis node
+            this.syncing = false;
+            return;
+        }
+
+        const peer = this.pick_any_peer();
+
+        const local_height = this.blockchain.chain.length - 1;
+        const blocks: BlockType[] = await rpc_client(peer.rpc).get_blocks_till_end(local_height + 1);
+
+        for(const block of blocks) {
+
+            if(!this.blockchain.is_valid_block(block)) {
+                throw new Error(chalk.red('ERROR: ') + 'invalid block during sync');
+            }
+
+            for(const tx of block.transactions) {
+                const result = this.runtime.execute_transaction(tx);
+                if(!result.ok) {
+                    throw new Error(chalk.red('ERROR: ') + 'invalid transaction during sync');
+                }
+            }
+
+            this.blockchain.add_block(block);
+        }
+
+        this.syncing = false;
+    }
+
+    private start_slot_clock() {
         this.tick_slot();
-        this.slot_timer = setInterval(
+        const slot_timer = setInterval(
             () => this.tick_slot(),
             this.slot_duration,
         );
@@ -74,7 +124,7 @@ export default class Node {
     }
 
     private is_leader(slot: number) {
-        return (slot % this.total_nodes) === this.node_id;
+        return this.validators[slot % this.validators.length]?.nodeId === this.node_id;
     }
 
     private produce_block() {
@@ -124,6 +174,35 @@ export default class Node {
             // add new block to the chain
             console.log(chalk.cyanBright('Block received: '), block.index);
         }
+    }
+
+    public get_peers() {
+        const existing_peers = Array.from(this.peers.values());
+        return existing_peers;
+    }
+
+    public on_peer_hello(peer_info: PeerType): { ok: boolean, err?: string } {
+        const adding_peer = this.add_peer(peer_info);
+        if(adding_peer) return { ok: true };
+        return { ok: false, err: `Peer with this node-id: ${peer_info.nodeId} already exists` };
+    }
+
+    public merge_peers(peer_list: PeerType[]) {
+        for(const peer of peer_list) {
+            this.add_peer(peer);
+        }
+    }
+    
+    private add_peer(peer_info: PeerType): boolean {
+        if(this.peers.has(peer_info.nodeId)) return false;
+        this.peers.set(peer_info.nodeId, peer_info);
+        return true;
+    }
+
+    private pick_any_peer(): PeerType {
+        const peer = this.peers.get(0);
+        if(!peer) throw new Error(chalk.red('ERROR: ') + 'no peers found');
+        return peer;
     }
 
 }
